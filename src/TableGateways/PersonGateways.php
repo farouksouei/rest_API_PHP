b0dc6df4d84d8c1ba7be5b96821b680e46760a8a
farouk <farouksouei@gmail.com>
2022-04-10 09:45:57 +0000

    added insert to php rest_api

--- src/TableGateways/PersonGateways.php
+++ src/TableGateways/PersonGateways.php
@@ -10,6 +10,7 @@ class PersonGateway
     $this->db = $db;
   }
 
+  //GET ALL RECORDS
   public function getAll()
   {
     $statement = "
@@ -27,7 +28,59 @@ class PersonGateway
       $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
       return $result;
     } catch (\PDOException $th) {
-      exit($th->getMessage("mamchetech"));
+      exit($th->getMessage("mamchetech el get all"));
+    }
+  }
+
+  //GET RECORD BY ID
+  public function getById($id)
+  {
+    $statement = "
+      SELECT
+        id,
+        firstname,
+        lastname,
+        firstparent_id,
+        secondparent_id
+      FROM person
+      WHERE id = ?";
+
+    try {
+      $statement = $this->db->prepare($statement);
+      $statement->execute([$id]);
+      $result = $statement->fetch(\PDO::FETCH_ASSOC);
+      return $result;
+    } catch (\PDOException $th) {
+      exit($th->getMessage("mamchetech el find bel id"));
+    }
+  }
+
+  //INSERT RECORD
+  public function insert(array $data)
+  {
+    $statement = "
+      INSERT INTO person
+        (firstname,
+        lastname,
+        firstparent_id,
+        secondparent_id)
+      VALUES
+        (:firstname,
+         :lastname,
+         :firstparent_id,
+         :secondparent_id)";
+
+    try {
+      $statement = $this->db->prepare($statement);
+      $statement->execute(array(
+        ':firstname' => $data['firstname'],
+        ':lastname' => $data['lastname'],
+        ':firstparent_id' => $data['firstparent_id'],
+        ':secondparent_id' => $data['secondparent_id']
+      ));
+      return $statement->rowCount();
+    } catch (\PDOException $th) {
+      exit($th->getMessage("mamchetech el insert"));
     }
   }
 }
